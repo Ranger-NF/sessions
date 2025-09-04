2f6bd6f5b5e0eec5ec59417f571c9d296cc15992
Fahad <78952725+Ranger-NF@users.noreply.github.com>
2025-09-04 20:21:58 +0530

    Added theme toggle and text animation

--- code/sngce-web/scripts.js
+++ code/sngce-web/scripts.js
@@ -9,3 +9,35 @@ if (hourOfDay > 6 && hourOfDay < 12) {
 } else {
     greeting.textContent = "Good Evening!"
 }
+
+const btn = document.getElementById("theme-toggle")
+let isLightMode = true;
+
+btn.addEventListener("click", () => {
+    if (isLightMode) {
+        isLightMode = false;
+        btn.textContent = "☀";
+    } else {
+        isLightMode = true;
+        btn.textContent = "🌙";
+    }
+    document.body.classList.toggle("dark-mode");
+})
+
+const tagline = document.getElementById("tagline");
+
+const text = "Web Developer";
+
+tagline.textContent = "";
+
+let i = 0;
+
+function typewriter() {
+    if (i <= text.length) {
+        tagline.textContent += text.charAt(i);
+        i++;
+        setTimeout(typewriter, 100);
+    }
+}
+
+typewriter();
